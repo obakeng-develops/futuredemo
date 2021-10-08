5c965f8c5777ae87cfce52f2af6e193530f51e8e
Obakeng <mosadiobakeng7@gmail.com>
2021-10-08 16:11:06 +0200

    Changed the navigation background to black as well as added navigation links.

--- api/frontend/src/components/Navigation.js
+++ api/frontend/src/components/Navigation.js
@@ -1,10 +1,27 @@
 import React, { Component } from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
 
 function Navigation() {
     return (
-        <div>
-            Yo
-        </div>
+        <>
+        <Navbar bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand href="#home">FutureForex</Navbar.Brand>
+                <Navbar.Toggle />
+                <Navbar.Collapse className="justify-content-end">
+                    <Nav className="ml-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#features">Notifications</Nav.Link>
+                        <Nav.Link href="#clients">Clients</Nav.Link>
+                        <Nav.Link href="#requests">Requests</Nav.Link>
+                        <Nav.Link href="#profile">Profile</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+        </>
     )
 }
 
