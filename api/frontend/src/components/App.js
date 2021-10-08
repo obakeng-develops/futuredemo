6496e698e9251ebd3808715efb4aa0ca62ad6988
Obakeng <mosadiobakeng7@gmail.com>
2021-10-08 16:52:02 +0200

    Added react-router-bootstrap for routing.

--- api/frontend/src/components/App.js
+++ api/frontend/src/components/App.js
@@ -1,15 +1,39 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import { render } from "react-dom";
 import Navigation from "./Navigation";
 import Container from "react-bootstrap/esm/Container";
+import Button from "react-bootstrap/Button";
+import LinkContainer from "react-router-bootstrap";
 
 export default function App() {
+
+    const [user, setUser] = useState('');
+
+    const getUser = async () => {
+        try {
+            const user = await fetch(`http://127.0.0.1:8000/api/user/profile/4`)
+            const data = await user.json()
+
+            setUser(data.first_name);
+        } catch (err) {
+            console.log(err.message);
+        }
+    };
+
+    useEffect(() => {
+        getUser();
+    }, []);
+
     return (
         <>
         <Navigation/>
         <div className="section">
             <Container>
-                <h1 className="my-5">Welcome Back, Jane.</h1>
+                <h1 className="mt-5 mb-3 large">Welcome Back, {user}.</h1>
+                <LinkContainer>
+                    <Button variant="primary">Create Request</Button>{' '}
+                </LinkContainer>
+                <Button variant="success">See Clients</Button>{' '}
             </Container>
         </div>
         </>
