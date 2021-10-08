a228672cf675b0268221743b5da6392a7235b865
Obakeng <mosadiobakeng7@gmail.com>
2021-10-08 16:15:06 +0200

    Added new productivity section to manager homepage.

--- api/frontend/src/components/App.js
+++ api/frontend/src/components/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
 import Navigation from "./Navigation";
+import Container from "react-bootstrap/esm/Container";
 
 export default function App() {
     return (
+        <>
         <Navigation/>
+        <div className="section">
+            <Container>
+                <h1 className="my-5">Welcome Back, Jane.</h1>
+            </Container>
+        </div>
+        </>
         )
 }
 
