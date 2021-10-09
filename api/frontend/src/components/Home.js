94c375bb6d510b3d179343818d8a8e8a216c9c64
Obakeng <mosadiobakeng7@gmail.com>
2021-10-09 20:54:34 +0200

    Added link to requests page on home page.

--- api/frontend/src/components/Home.js
+++ api/frontend/src/components/Home.js
@@ -7,8 +7,8 @@ function Home({user}) {
     return (
         <Container>
                 <h1 className="mt-5 mb-3 large">Welcome Back, {user}.</h1>
-                <Link to="/clients"><Button variant="primary">Create Request</Button>{' '}</Link>
-                <Button variant="success">See Clients</Button>{' '}
+                <Link to="/requests"><Button variant="primary">Create Request</Button>{' '}</Link>
+                <Link to="/clients"><Button variant="success">See Clients</Button>{' '}</Link>
         </Container>
     )
 }
