074d1e47e4ec60257ccbe07c0680f67c79057bd9
Obakeng <mosadiobakeng7@gmail.com>
2021-10-09 20:50:38 +0200

    Fetched data for all the manager's clients onto the dashboard.

--- api/frontend/src/components/Clients.js
+++ api/frontend/src/components/Clients.js
@@ -1,23 +1,27 @@
 import React, { useState, useEffect } from "react";
 import Container from "react-bootstrap/esm/Container";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
 function Clients() {
 
     const [clients, setClients] = useState([]);
 
-    const getClients = async () => {
-        try {
-            const clients = await fetch(`http://127.0.0.1:8000/client/membership/manager/4`)
-            const data = await clients.json();
-
-            setClients(data);
-        } catch (err) {
-            console.log(err.message);
-        }
-    };
-
     useEffect(() => {
-        getClients();
+        fetch(`http://127.0.0.1:8000/api/client/membership/manager/4`)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw response;
+        })
+        .then (data => {
+            console.log(data);
+            setClients(data);
+        })
+        .catch(error => {
+            console.error("Error fetching data: ", error);
+        })
     }, []);
 
     return (
@@ -25,7 +29,22 @@ function Clients() {
         <div className="mt-5">
             <Container>
                 <div className="">
-                    <h1 className="large">Clients</h1>
+                    <h1 className="large">Your Clients</h1>
+                    {
+                        clients.map(client => {
+                            return (
+                                <>
+                                <Card className="w-50 my-3 shadow-sm">
+                                    <div className="flex p-4 flex-inline">
+                                        <h1 className="h3" key={client.client.id}>{client.client.email}</h1>
+                                        <h1 className="h5 text-muted" key={client.group.client_group_name}>Date joined: {client.date_joined}</h1>
+                                        <Button variant="primary">View</Button>
+                                    </div>
+                                </Card>
+                                </>
+                            )
+                        })
+                    }
                 </div>
             </Container>
         </div>
