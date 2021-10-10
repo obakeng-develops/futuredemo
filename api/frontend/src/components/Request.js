00f6e968030ae79603794cd293536cdfb73e2920
Obakeng <mosadiobakeng7@gmail.com>
2021-10-10 16:34:36 +0200

    Added Request page.

---
+++ api/frontend/src/components/Request.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Card from "react-bootstrap/Card";
+
+function Request() {
+    return (
+        <>
+        <Container>
+            <div className="mt-5">
+                <h1 className="large">Your Requests</h1>
+                <Card>
+                    
+                </Card>
+            </div>
+        </Container>
+        </>
+    )
+}
+
+export default Request
