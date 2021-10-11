b7503dedae439159259ded3cd729ee775fd7f44b
Obakeng <mosadiobakeng7@gmail.com>
2021-10-11 09:46:30 +0200

    Refactored views for Manager app.

--- api/manager/urls.py
+++ api/manager/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from .views import (
     RequestAPIView,
+    CreateRequestAPIView,
     RequestDetailAPIView
 )
 
 urlpatterns = [
     path("requests", RequestAPIView.as_view()),
-    path("requests/client/<int:user_id>", RequestDetailAPIView.as_view())
+    path("requests/create", CreateRequestAPIView.as_view()),
+    path("requests/<int:pk>", RequestDetailAPIView.as_view())
 ]
